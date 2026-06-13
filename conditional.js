567ac1b5a05732a522538f2c929cb02f9c78d28d
Imrana-25 <m.imrana0625@gmail.com>
2026-06-13 13:47:37 +0530

    added tables.js

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var colour="white"
+if(colour == "green"){
+    console.log("leaf")
+}
+elseif(colour==" red"){
+    console.log("rose")
+}
+elseif(colour=="purple"){
+    console.log("daizy")
+}
+elseif(colour=="yellow"){
+    console.log("golden flower")
+
+}
+elseif(colour=="pink"){
+    console.log("lotus")
+}
+else{
+    console.log("white rose" )
+}
+console.log("invalid colour")
---
+++ function.js
@@ -0,0 +1,12 @@
+//non paramatric function
+function greet(){
+    console.log("hello");
+
+}
+greet();
+
+//paramatric function
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,3));
